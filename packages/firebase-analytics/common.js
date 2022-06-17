faacaa120b64ab040b623dd4492b5dda08647303
Meg Seegmiller <mseegmiller@hhomesltd.com>
2022-06-17 11:55:52 -0500

    Publish JS

---
+++ packages/firebase-analytics/common.js
@@ -0,0 +1,11 @@
+export var ConsentType;
+(function (ConsentType) {
+	ConsentType[(ConsentType['Ad_Storage'] = 0)] = 'Ad_Storage';
+	ConsentType[(ConsentType['Analytics_Storage'] = 1)] = 'Analytics_Storage';
+})(ConsentType || (ConsentType = {}));
+export var ConsentStatus;
+(function (ConsentStatus) {
+	ConsentStatus[(ConsentStatus['Denied'] = 0)] = 'Denied';
+	ConsentStatus[(ConsentStatus['Granted'] = 1)] = 'Granted';
+})(ConsentStatus || (ConsentStatus = {}));
+//# sourceMappingURL=common.js.map
